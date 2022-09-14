109fb6488412d3b168c9344d5ed6b62f26590e1c
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2022-09-13 21:24:27 -0400

    modifying well known to support #420

--- test/e2e/test_infra_configs.py
+++ test/e2e/test_infra_configs.py
@@ -4,11 +4,17 @@ from playwright.sync_api import APIRequestContext
 def test_matrix_well_known(
     api_request_context: APIRequestContext,
 ):
-    well_known_path = "/.well-known/matrix/server"
-    response = api_request_context.get(well_known_path)
+    well_known_folder = "/.well-known/matrix"
+    well_known_types = {
+        "client": {
+            "m.homeserver": {"base_url": "https://colony.jupiterbroadcasting.com"}
+        },
+        "server": {"m.server": "colony.jupiterbroadcasting.com:443"},
+    }
 
-    assert response.ok, "200-299 status code"
-    assert response.body() != "", "response is not empty"
-    assert response.json() == {
-        "m.server": "colony.jupiterbroadcasting.com:443"
-    }, "expected reponse for JB matrix"
+    for well_known_key, well_known_data in well_known_types.items():
+        response = api_request_context.get(f"{well_known_folder}/{well_known_key}")
+
+        assert response.ok, "200-299 status code"
+        assert response.body() != "", "response is not empty"
+        assert response.json() == well_known_data, "expected reponse for JB matrix"
