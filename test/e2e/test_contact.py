a8b9fab3a250e3c22651d7ff9282bba64daece57
Ryan Goodwin <goodwinryan2@gmail.com>
2022-10-07 14:18:20 -0400

    ✅ Add test for iframe size

--- test/e2e/test_contact.py
+++ test/e2e/test_contact.py
@@ -1,4 +1,4 @@
-from pytest import mark
+import time
 from pathlib import Path
 from playwright.sync_api import Page, expect
 
@@ -8,7 +8,6 @@ def test_contact_screenshot(page: Page, screenshot_dir: Path):
     page.pause()
     page.screenshot(path=f"{screenshot_dir}/contact.png", full_page=True)
 
-@mark.skip(reason="Not finished implementing")
 def test_submit_button_visible(page: Page, screenshot_dir: Path):
     page.goto("/contact")
     contact_form: FrameLocator = page.frame_locator('#contact-frame')
@@ -17,16 +16,16 @@ def test_submit_button_visible(page: Page, screenshot_dir: Path):
     submit_button.click()
     page.wait_for_load_state("networkidle")
 
-    frame_height: int = int(page.get_attribute(selector="#contact-frame", name="height"))
+    # Scroll to the top (Page is current about 1400px)
+    page.mouse.wheel(0.0, -1500.0)
+    time.sleep(1) # Give time to scroll to the top
 
-    # Bounding Box is relative to view port, making this test invalid.
-    # Need to find a way to get the bottom of the submit button in
-    # pixels on the form to check if it is greater than the size of
-    # the iframe
+    frame_height: float = float(page.get_attribute(selector="#contact-frame", name="height")) + page.locator(selector="#contact-frame").bounding_box().get("y")
+
     button_location: dict = submit_button.bounding_box()
     if button_location is None:
         assert False, "Submit Button is hidden"
     button_bottom: float = button_location.get("y") + button_location.get("height")
 
-    assert form_height <= frame_height, "Form content is running off the iframe"
-    page.screenshot(path=f"{screenshot_dir}/contact_errors.png")
+    page.screenshot(path=f"{screenshot_dir}/contact_errors.png", full_page=True)
+    assert button_bottom <= frame_height, "Form content is running off the iframe"
