0c885cec00051a861e57774891c1cd7d7412999c
Ryan Goodwin <goodwinryan2@gmail.com>
2022-10-26 04:02:52 -0400

    ✅ Add test for 404 page

---
+++ test/e2e/test_404.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from pytest import fixture
+from playwright.sync_api import Page, expect
+
+
+"""
+Code here will run for every test in this file
+"""
+@fixture(autouse=True)
+def setup(page: Page):
+    page.goto("/404.html")
+
+
+"""
+Take a screenshot of the 404 page for manual review
+"""
+def test_404_screenshot(page: Page, screenshot_dir: Path):
+    page.screenshot(path=f"{screenshot_dir}/404.png", full_page=True)
+
+
+"""
+Ensure text "Page Not Found" and unicorn image is present on the 404 page
+"""
+def test_404_contents(page: Page):
+    title = page.locator(".title.has-text-centered")
+    expect(title).to_contain_text("Page Not Found")
+
+    unicorn = page.locator("#img-404")
+    expect(unicorn).to_be_visible()
