a44417e3e63d9b641fe7cde4a04a741981a5d486
Ryan Goodwin <goodwinryan2@gmail.com>
2022-11-03 12:33:46 -0400

    :white_check_mark: Add test suggestions from review

--- test/external/test_matrix.py
+++ test/external/test_matrix.py
@@ -5,4 +5,6 @@ from playwright.sync_api import APIRequestContext
 @mark.periodic
 def test_matrix_federation(api_request_context: APIRequestContext):
     response = api_request_context.get("https://federationtester.matrix.org/api/report?server_name=jupiterbroadcasting.com")
-    assert response.json()["FederationOK"] == True
+    assert response.ok, "API did not return a response code of 200-299"
+    assert response.body() != "", "API did not return any data"
+    assert response.json()["FederationOK"] == True, "Federation is not okay"
