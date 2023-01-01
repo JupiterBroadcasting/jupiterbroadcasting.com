dbc4e7db8f304904b349940babf9148d02b8cc98
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2023-01-01 16:27:40 -0500

    adding test for mobile live indicators

--- test/e2e/test_live.py
+++ test/e2e/test_live.py
@@ -1,6 +1,6 @@
 from typing import Tuple, Callable
 from pytest import mark
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Locator
 
 
 def test_live_indicator(
@@ -35,4 +35,21 @@ def test_mobile_live_indicator(
 
     mobile_device.goto("/live")
 
-    assert False
+    navbar: Locator = mobile_device.locator("#mainnavigation.is-live").locator('.navbar-burger')
+    # wait for navbar to be visible
+    expect(navbar).to_be_visible()
+    # check if live indicator is red
+    assert navbar.evaluate(
+        # integrated docs for python evaluate function: https://github.com/microsoft/playwright/blob/a30aac56687598c373c51255308ef5833de0c9bb/docs/src/api/class-jshandle.md?plain=1#L77-L80
+        # use evaluate function in python: https://playwright.dev/python/docs/api/class-page#page-evaluate
+        # use getComputerStyle w/playwright: https://stackoverflow.com/a/71433333
+        "element => window.getComputedStyle(element, ':before').backgroundColor"
+    ) == "rgb(255, 0, 0)"
+    
+    navbar.click()
+
+    # repeat test_live_indicator for mobile device
+    expect(mobile_device.locator("#mainnavigation.is-live").locator("#livebutton"),).to_have_css(
+        name="background-color",
+        value="rgb(255, 0, 0)",  # red
+    )
