56257a666da1f2f4bf1186167061d4bcd787418f
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2023-01-01 13:41:44 -0500

    adding more docs and an actual test

--- test/e2e/test_live.py
+++ test/e2e/test_live.py
@@ -1,20 +1,22 @@
-from time import sleep
 from typing import Tuple, Callable
 from pytest import mark
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
-@mark.dev
-def test_check_live(
+def test_live_indicator(
     page: Page,
     set_live: Tuple[Callable, str],
 ):
+    # explicitly defining tuple objects for clarity
     replace_live_event: Callable = set_live[0]
     live_event: str = set_live[1]
 
     # intercepting reponses for live event
     replace_live_event(page, live_event)
 
-    page.goto('/live')
-    sleep(30)
-    assert False
+    page.goto("/live")
+
+    expect(page.locator("#mainnavigation.is-live").locator("#livebutton"),).to_have_css(
+        name="background-color",
+        value="rgb(255, 0, 0)",  # red
+    )
