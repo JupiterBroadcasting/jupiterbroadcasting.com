7d3f9b6d7f13fd411ef39a25dbb1744f3d21b29e
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2023-01-01 22:14:48 -0500

    formatting code w/black

--- test/e2e/test_live.py
+++ test/e2e/test_live.py
@@ -21,6 +21,7 @@ def test_live_indicator(
         value="rgb(255, 0, 0)",  # red
     )
 
+
 def test_mobile_live_indicator(
     mobile_device: Page,
     set_live: Tuple[Callable, str],
@@ -34,21 +35,32 @@ def test_mobile_live_indicator(
 
     mobile_device.goto("/live")
 
-    navbar: Locator = mobile_device.locator("#mainnavigation.is-live").locator('.navbar-burger')
+    navbar: Locator = mobile_device.locator("#mainnavigation.is-live",).locator(
+        ".navbar-burger",
+    )
+
     # wait for navbar to be visible
     expect(navbar).to_be_visible()
+
     # check if live indicator is red
-    assert navbar.evaluate(
-        # integrated docs for python evaluate function: https://github.com/microsoft/playwright/blob/a30aac56687598c373c51255308ef5833de0c9bb/docs/src/api/class-jshandle.md?plain=1#L77-L80
-        # use evaluate function in python: https://playwright.dev/python/docs/api/class-page#page-evaluate
-        # use getComputerStyle w/playwright: https://stackoverflow.com/a/71433333
-        "element => window.getComputedStyle(element, ':before').backgroundColor"
-    ) == "rgb(255, 0, 0)"
-    
+    assert (
+        navbar.evaluate(
+            # integrated docs for python evaluate function: https://github.com/microsoft/playwright/blob/a30aac56687598c373c51255308ef5833de0c9bb/docs/src/api/class-jshandle.md?plain=1#L77-L80
+            # use evaluate function in python: https://playwright.dev/python/docs/api/class-page#page-evaluate
+            # use getComputerStyle w/playwright: https://stackoverflow.com/a/71433333
+            "element => window.getComputedStyle(element, ':before').backgroundColor"
+        )
+        == "rgb(255, 0, 0)"
+    )
+
     navbar.click()
 
     # repeat test_live_indicator for mobile device
-    expect(mobile_device.locator("#mainnavigation.is-live").locator("#livebutton"),).to_have_css(
+    expect(
+        mobile_device.locator("#mainnavigation.is-live",).locator(
+            "#livebutton",
+        ),
+    ).to_have_css(
         name="background-color",
         value="rgb(255, 0, 0)",  # red
     )
