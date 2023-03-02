7ce7e46d60c76dc68f96ba403ca575025e554e73
Ryan Goodwin <goodwinryan2@gmail.com>
2023-03-01 20:33:26 -0500

    :test_tube: update tag test to look at episode list (failing)

--- test/e2e/test_episode.py
+++ test/e2e/test_episode.py
@@ -16,6 +16,10 @@ EXTRA_EPISODES: list[dict[str, str]] = [
     {  # No Sponsor
         "show": "coder-radio",
         "number": "343"
+    },
+    {  # Emoji tags
+        "show": "linux-unplugged",
+        "number": "489"
     }
 ]
 
@@ -36,33 +40,46 @@ def episodes() -> list[str]:
     # Fist and last 5 episodes for each show
     for show in os.listdir(shows_dir):
         show_url: str = f"/show/{show}"
-        all_episodes: list[str] = [e for e in os.listdir(f"{shows_dir}/{show}") if os.path.isfile(f"{shows_dir}/{show}/{e}") and e not in meta_files]
+        all_episodes: list[str] = [
+            e for e in os.listdir(f"{shows_dir}/{show}")
+            if os.path.isfile(f"{shows_dir}/{show}/{e}") and e not in meta_files
+        ]
         all_episodes.sort()
 
-        test_episodes += [f"{show_url}/{os.path.splitext(e)[0].lstrip('0') or '0'}" for e in all_episodes[:5:] if e not in test_episodes]  # First 5 episodes of a show
-        test_episodes += [f"{show_url}/{os.path.splitext(e)[0].lstrip('0') or '0'}" for e in all_episodes[-5::] if e not in test_episodes]  # 5 most recent episodes of a show
+        # First 5 episodes of a show
+        test_episodes += [
+            f"{show_url}/{os.path.splitext(e)[0].lstrip('0') or '0'}"
+            for e in all_episodes[:5:] if e not in test_episodes
+        ]
+        # 5 most recent episodes of a show
+        test_episodes += [
+            f"{show_url}/{os.path.splitext(e)[0].lstrip('0') or '0'}"
+            for e in all_episodes[-5::] if e not in test_episodes
+        ]
 
     # Defined list of edge case episodes
-    test_episodes += [f"/show/{e['show']}/{e['number']}" for e in EXTRA_EPISODES if e not in test_episodes]
+    test_episodes += [
+        f"/show/{e['show']}/{e['number']}"
+        for e in EXTRA_EPISODES if e not in test_episodes
+    ]
 
     return test_episodes
 
 
-@mark.dev
-def test_page_title(page: Page, episodes: list[str]):
+def test_episode_page_title(page: Page, episodes: list[str]):
     """
     Make sure the title (text in the tab) contains the show name, episode number, and
     network name (Jupiter Broadcasting)
     """
     # TODO figure out how to call this loop async
     for url in episodes:
-        show, episode_number = url.split('/')[2:4] # This skips the empty string and 'show' in the url
+        show, episode_number = url.split('/')[2:4]  # This skips the empty string and 'show' in the url
         page.goto(url)
         print(f"{show} - {episode_number}")
         expect(page).to_have_title(re.compile(f".* | {show} {episode_number} | Jupiter Broadcasting"))
 
 
-def test_podverse_player(page: Page, episodes: list[str]):
+def test_episodes_podverse_player(page: Page, episodes: list[str]):
     """
     Test podverse player to and make sure the correct show is displayed
     """
@@ -80,10 +97,12 @@ def test_podverse_player(page: Page, episodes: list[str]):
         check_title(page, url)
 
 
-def test_tag_broken_link_spaces(page: Page):
+def test_episode_tag_broken_link_spaces(page: Page, episodes: list[str]):
     """
     Checking for broken tag links with spaces in them
     """
-    page.goto("/show/linux-unplugged/489/")
-    page.locator(".tag > a", has_text="linux unplugged").click()
-    expect(page.locator(".title", has_text="Tag: linux unplugged")).to_be_visible()
+    for url in episodes:
+        show, episode_number = url.split('/')[2:4]  # This skips the empty string and 'show' in the url
+        page.goto(url)
+        page.locator(".tag > a", has_text=show).click()
+        expect(page.locator(".title", has_text=f"Tag: {show}")).to_be_visible()
